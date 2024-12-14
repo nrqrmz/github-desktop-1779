3a043b471d1148b0898ff723d8e70a73b3a8230a
Enrique Ramírez Ramos <nrqrmz@gmail.com>
2024-12-14 03:47:47 -0600

    add solution for challenge 4

---
+++ queries/challenge4.sql
@@ -0,0 +1,24 @@
+with product_total as
+(SELECT
+  customerid,
+  productid,
+  round(sum(unitprice * quantity),2) as total_value 
+FROM OrderDetails
+JOIN Orders USING (orderid)
+group by 1, 2),
+
+ranked_prod as 
+(SELECT
+ 	customerid,
+ 	productid,
+ 	total_value,
+ 	rank() over (partition by customerid order by total_value DESC) as rnk
+ FROM product_total)
+
+
+SELECT
+  customerid,
+  productid,
+  total_value as OrderedAmount	
+FROM ranked_prod
+where rnk = 1   
